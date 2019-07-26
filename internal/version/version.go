1488e83e4fb5b310dc9e096dbdba10992c6c1083
Scott Crawford <scrawford@digitalocean.com>
2019-07-26 13:45:54 -0400

    initial commit to felix

---
+++ internal/version/version.go
@@ -0,0 +1,10 @@
+package version
+
+const (
+	currentVersion = "0.1.0"
+)
+
+// CurrentVersion returns the most recent stable version of felix
+func CurrentVersion() string {
+	return currentVersion
+}
