cf3785ed120fb869ff8a1daf349f52d4df6848ea
Scott Crawford <scrawford@squareup.com>
2021-09-08 17:05:47 -0600

    remove fmt print and handle err

--- internal/builder/file_handler.go
+++ internal/builder/file_handler.go
@@ -16,7 +16,6 @@ func (b *Builder) writeToLocal() error {
 		destinationDir = fmt.Sprintf("%s/", b.template.Name)
 	}
 
-	fmt.Println("read items from tmp dir")
 	filepath.WalkDir(b.rootDir, func(path string, d fs.DirEntry, _ error) error {
 		if path == b.rootDir {
 			return nil
@@ -33,10 +32,8 @@ func (b *Builder) writeToLocal() error {
 
 		if fileInfo.IsDir() {
 			truePath := strings.Replace(path, fmt.Sprintf("%s/", b.rootDir), destinationDir, 1)
-			fmt.Println("what is this? ", truePath)
 			err := os.MkdirAll(truePath, fileInfo.Mode())
 			if err != nil {
-				fmt.Println("error making dir: ", err.Error())
 				return err
 			}
 
@@ -49,7 +46,10 @@ func (b *Builder) writeToLocal() error {
 			b.updateTemplateFromFelixYaml()
 		}
 
-		b.writeFile(path, truePath)
+		err = b.writeFile(path, truePath)
+		if err != nil {
+			return err
+		}
 
 		return nil
 	})
@@ -60,41 +60,18 @@ func (b *Builder) writeToLocal() error {
 func (b *Builder) writeFile(tmpPath, newPath string) error {
 	file, err := ioutil.ReadFile(tmpPath)
 	if err != nil {
-		fmt.Println("err reading file ", tmpPath)
 		return err
 	}
 
 	parsedFile, err := b.parseTemplate(string(file))
 	if err != nil {
-		fmt.Println("err parsing: ", err)
+		return err
 	}
 
-	fmt.Println("writing file to path", newPath)
 	err = ioutil.WriteFile(newPath, parsedFile, 0644)
 	if err != nil {
-		fmt.Println("err writing file: ", err.Error())
 		return err
 	}
 
 	return nil
 }
-
-// func (b *Builder) writeFile(destinationDir, fileName string) error {
-// 	file, err := b.Box.FindString(fileName)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// parsedFile, err := b.parseTemplate(file)
-// if err != nil {
-// 	fmt.Println("err parsing: ", err)
-// }
-
-// newFile := filepath.Join(destinationDir, fileName)
-// err = ioutil.WriteFile(newFile, parsedFile, 0644)
-// if err != nil {
-// 	return err
-// }
-
-// 	return nil
-// }
